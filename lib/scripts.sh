2f5eddea507571828f0d977461bc88e0e7524495
Aleksander Pentchev <alek.pentchev@gmail.com>
2022-10-26 14:39:58 +0200

    update scripts and gitignore

--- lib/scripts.sh
+++ lib/scripts.sh
@@ -4,6 +4,9 @@ aws cloudformation list-stack-resources --stack-name LambdaJsonCsvStack --endpoi
 # List Lambda functions
 aws lambda list-functions --endpoint-url=http://localhost:443
 
+# Invoke Lambda function
+aws lambda invoke output.json --function-name LambdaJsonCsvStack-functionconstructlambdat-6b63f92e --endpoint-url=http://localhost:443
+
 # List S3 Buckets
 aws s3 ls --endpoint-url=http://localhost:443
 
@@ -11,10 +14,19 @@ aws s3 ls --endpoint-url=http://localhost:443
 aws s3 ls s3://lambdajsoncsvstack-bucketconstructbucketjso-73dbcc34 --endpoint-url=http://localhost:443
 
 # Put dummy file in S3 Bucket
-aws s3 cp ./dummyfile2.json s3://lambdajsoncsvstack-bucketconstructbucketjso-73dbcc34 --endpoint-url=http://localhost:443
+aws s3 cp ./dummyfile.json s3://lambdajsoncsvstack-bucketconstructbucketjso-73dbcc34 --endpoint-url=http://localhost:443
 
 # Tail Lambda logs
-aws logs tail '/aws/lambda/LambdaJsonCsvStack-BucketNotificationsHandl-6c6eaf12' --follow --endpoint-url=http://localhost:443
+aws logs tail '/aws/lambda/LambdaJsonCsvStack-functionconstructlambdat-6b63f92e' --follow --endpoint-url=http://localhost:443
 
 # List CloudWatch log groups
 aws logs describe-log-groups --endpoint-url=http://localhost:443
+
+# List CloudWatch log streams
+aws logs describe-log-streams --log-group-name '/aws/lambda/LambdaJsonCsvStack-functionconstructlambdat-6b63f92e' --endpoint-url=http://localhost:443
+
+# Get CloudWatch log events
+aws logs get-log-events --log-group-name '/aws/lambda/LambdaJsonCsvStack-functionconstructlambdat-6b63f92e' --log-stream-name '2022/10/26/[LATEST]3f5ac587' --endpoint-url=http://localhost:443
+
+# Describe log groups
+aws logs describe-log-groups --log-group-name-prefix LambdaJsonCsvStack-BucketNotificationsHandl-6c6eaf12 --endpoint-url=http://localhost:443
